ab40a02f28aeb016fd862a306c7f9b3236ff9a7d
bhargav-BL <bhargavarampentyala@gmail.com>
2025-08-16 17:16:14 +0530

    xxss

--- components/CookieBanner.tsx
+++ components/CookieBanner.tsx
@@ -27,7 +27,7 @@ const CookieBanner = () => {
 	if (!visible) return null;
 
 	return (
-		<div className="fixed bottom-4 left-4 right-4 z-50 max-w-sm rounded-lg bg-white shadow-xl border-2 border-gray-300 p-4">
+		<div className="fixed bottom-4 left-4 right-4 z-50 max-w-sm rounded-xl bg-white shadow-xl border-2 border-gray-300 p-4">
 			<div className="flex items-start gap-3">
 				<CookieIcon size={28} weight="fill" className="text-[#0b9344] mt-1" />
 				<div className="flex-1 text-sm text-gray-700">
@@ -35,22 +35,19 @@ const CookieBanner = () => {
 						We use cookies to enhance your experience. By continuing to browse, you agree to our use
 						of cookies.
 					</p>
-					<div className="mt-3 flex items-center justify-end gap-4 ">
-						<button
-							onClick={handleAccept}
-							className="cta px-4 py-2 rounded-md bg-slate-950 text-white text-sm font-semibold shadow-lg flex items-center justify-center gap-2 cursor-pointer">
-							Accept
-						</button>
-						<Link href="/privacy-policy" className="text-sm">
-							Privacy Policy
-						</Link>
-					</div>
 				</div>
 				<button
 					onClick={handleClose}
 					aria-label="Close cookie banner"
 					className="text-gray-800 cursor-pointer transition">
 					<XIcon size={20} />
+				</button>
+			</div>
+			<div className="mt-3 flex items-center justify-end gap-4 ">
+				<button
+					onClick={handleAccept}
+					className="cta px-4 py-2 rounded-md bg-slate-950 text-white text-sm font-semibold shadow-lg flex items-center justify-center gap-2 cursor-pointer">
+					Accept
 				</button>
 			</div>
 		</div>
