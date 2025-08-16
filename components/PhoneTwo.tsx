bd542a475be06984f7588fd31ef2cac07bc01e9b
bhargav-BL <bhargavarampentyala@gmail.com>
2025-08-16 17:12:30 +0530

    qws

--- components/PhoneTwo.tsx
+++ components/PhoneTwo.tsx
@@ -5,15 +5,16 @@ const PhoneTwo = () => {
 		<div>
 			<img
 				src="/mobile.png"
-				alt="two-mobiles-mockup - One App. All Your Health Needs."
+				alt="two-mobiles-mockup - All Your Complete Health Records in One Secure App."
 				className="w-[90vw] md:w-[50vw] mx-auto mt-20"
 			/>
 			<h1 className="text-center -mt-2 md:text-5xl text-2xl font-bold tracking-tighter px-4 text-balance">
-				One App. All Your Health Needs.
+				All Your Complete Health Records in One Secure App
 			</h1>
 			<p className="text-center text-balance text-gray-600 px-4">
-				MyHealthNotion makes it easy to stay on top of your health by tracking everything in one
-				place.
+				My Health Notion helps you store and manage digital health records—medical reports,
+				prescriptions, insurance, and vaccination details. Get AI insights, graphical health trends,
+				and share instantly with doctors, family, or in emergencies.
 			</p>
 		</div>
 	);
