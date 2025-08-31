e87ba2c4a2aa350591c9cb8b27488b9739968991
bhargav-BL <bhargavarampentyala@gmail.com>
2025-08-31 21:16:15 +0530

    sadsad

--- components/TestimonialsInner.tsx
+++ components/TestimonialsInner.tsx
@@ -1,30 +1,29 @@
 import React from "react";
 interface Props {
-  name: string;
-  tag: string;
-  text: string;
-  image: string;
+	name: string;
+	tag: string;
+	text: string;
+	image: string;
 }
 
 const Testimonials: React.FC<Props> = ({ name, tag, text, image }) => {
-  return (
-    <div className="flex flex-col items-center justify-center text-center md:p-6 p-1 md:max-w-240 max-w-screen">
-      <h1 className="font-semibold tracking-tight leading-[1.3] md:text-2xl text-xs">
-        {text}
-      </h1>
-      <div className="flex items-center mt-4">
-        <img
-          src={image}
-          alt={name}
-          className="testimonials-image w-12 h-12 rounded-full mr-4"
-        />
-        <div className="flex flex-col items-start ">
-          <h2 className="md:text-lg text-md font-semibold leading-5">{name}</h2>
-          <p className="md:text-sm text-xs text-gray-500 leading-5">{tag}</p>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col items-center justify-center text-center md:p-6 p-1 md:max-w-240 max-w-screen">
+			<h1 className="font-semibold tracking-tight leading-[1.3] md:text-2xl text-xs">{text}</h1>
+			<div className="flex items-center mt-4">
+				<img
+					src={image}
+					alt={name}
+					loading="lazy"
+					className="testimonials-image w-12 h-12 rounded-full mr-4"
+				/>
+				<div className="flex flex-col items-start ">
+					<h2 className="md:text-lg text-md font-semibold leading-5">{name}</h2>
+					<p className="md:text-sm text-xs text-gray-500 leading-5">{tag}</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Testimonials;
